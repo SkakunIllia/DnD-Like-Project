a0297a7202dbb58623ae8b35162c8aed50ae8714
Iliya Skakun <skakuniliya@gmail.com>
2026-05-07 19:58:22 +0200

    Add logging system. Start to add documentation for main.py and entities.py

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Author: Illia Skakun.
 This is university project.
 The idea of the project is a game that is called Dungeons&Dragons (DnD).
 Actually, the concept of a game stayed the same, but with little changes.
+
+File structure:
+1. main.py
+    - Main file to execute. Contains invocations to different functions and creates objects for the game.
+    
+        Functions:
+        
+        - log() 
+            logging function, used for initializing logging system that is used throughout the whole game
+        - 
+2. Entities.py
+    - Contains classes for every object and a function
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import Entities
+import logging
+import os
+from datetime import datetime
+from glob import glob
+
+def log():
+    path = "logs/*.log"
+    files = glob(path)
+    if len(files) >= 5:
+        os.remove(files[0])
+
+    log_dir = "logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+
+    date_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".log"
+    log_path = os.path.join(log_dir, date_now)
+    logging.basicConfig(level = logging.DEBUG, filename = log_path, filemode = "w", format = "%(asctime)s - %(levelname)s - %(message)s")
+    logging.debug("Test")
+
 
 def print_player_classes():
     print("1. Archer")
@@ -16,16 +36,23 @@ def read_player_class():
     try:
         while not (player_class := int(input("What class do you want to play? (Enter a number) "))):
             continue
+        match player_class:
+            case 1: player_class = "Archer"
+            case 2: player_class = "Wizard"
+            case 3: player_class = "Gnome"
+            case 4: player_class = "Ogr"
+            case 5: player_class = "Knight"
+            case _: raise ValueError("Wrong player_class. Usage: 1 <= player_class <= 5")
         return player_class
     except ValueError:
         return read_player_class()
 
 def main():
+    log()
     player_name = read_player_name()
     print_player_classes()
     player_class = read_player_class()
     player = Entities.Player(player_name, player_class)
     print(player)
-
 
 main()
