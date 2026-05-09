eef818269729bbcbb315567513c02f96ebce1840
Skakun Illia <skakuniliya@gmail.com>
2026-05-09 20:14:34 +0200

    Add description and fields to classes

--- Entities.py
+++ Entities.py
@@ -1,72 +1,118 @@
 from random import randint
 
+
 # Randomising value for the quest to be completed,
-# if the value of the player`s 'luck' is higher than
+# if the value of the player's 'luck' is higher than
 # the value it means that the quest has to be completed
 def random():
     return randint(randint(20, 40), randint(50, 70))
 
+def initialize_entity(player_class, player_name):
+    match player_class:
+        case 1: return Archer(player_name)
+        case 2: return Wizard(player_name)
+        case 3: return Gnome(player_name)
+        case 4: return Ogr(player_name)
+        case 5: return Knight(player_name)
+        case _: return Player(player_name, "Not defined")
+
+#===================================================================================
 class Entity:
-    def __init__(self, name, type):
+    def __init__(self, name):
         self._name = name
-        self._type = type
     def __repr__(self):
-        return f'Entity[Name: {self._name}, type: {self._type}]'
+        return f'Entity[Name: {self._name}]'
+    def set_name(self, name):
+        self._name = name
+    def get_name(self):
+        return self._name
 
 class Player(Entity):
     def __init__(self, name, player_class):
-        super().__init__(name, "Player")
+        super().__init__(name)
         self._player_class = player_class
         self._items = []
         self._health = 100
         self._weapon = None
     def __repr__(self):
-        return f'Player[Name: {self._name}, type: {self._type}, health:]'
+        return f'Player[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def set_health(self, health):
+        self._health = health
+    def get_health(self):
+        return self._health
+    def set_items(self, items):
+        self._items = items
+    def get_items(self):
+        return self._items
+    def get_player_class(self):
+        return self._player_class
 
 class Archer(Player):
     def __init__(self, name):
         super().__init__(name, "Archer")
-        self._health = 90
+        self.set_items([Item("Relict bow", "Bow", 65, 10)])
+        self.set_health(80)
         self._damage = 15
-        self._items = [Item("Relict bow", "Bow", 65, 10)]
+    def __repr__(self):
+        return f'Archer[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def get_damage(self):
+        return self._damage
 
 class Knight(Player):
     def __init__(self, name):
         super().__init__(name, "Knight")
-        self._health = 120
+        self.set_health(120)
+        self.set_items([Item("Silver sword", "Sword", 45, 13)])
         self._damage = 20
-        self._items = [Item("Silver sword", "Sword", 45, 13)]
+    def __repr__(self):
+        return f'Knight[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def get_damage(self):
+        return self._damage
 
 class Ogr(Player):
     def __init__(self, name):
         super().__init__(name, "Ogr")
-        self._health = 150
+        self.set_health(150)
+        self.set_items([Item("Greeny mace", "Mace", 80, 7)])
         self._damage = 30
-        self._items = [Item("Greeny mace", "Mace", 80, 7)]
+    def __repr__(self):
+        return f'Ogr[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def get_damage(self):
+        return self._damage
 
 class Gnome(Player):
     def __init__(self, name):
         super().__init__(name, "Gnome")
-        self._health = 100
+        self.set_health(100)
+        self.set_items([Item("Gold pickaxe", "Pickaxe", 50, 7)])
         self._damage = 20
-        self._items = [Item("Gold pickaxe", "Pickaxe", 50, 7)]
+    def __repr__(self):
+        return f'Gnome[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def get_damage(self):
+        return self._damage
 
 class Wizard(Player):
     def __init__(self, name):
         super().__init__(name, "Wizard")
-        self._health = 80
+        self.set_health(80)
+        self.set_items([Item("Magical wand", "Wand", 60, 15)])
         self._damage = 10
-        self._items = [Item("Magical wand", "Wand", 60, 15)]
+    def __repr__(self):
+        return f'Gnome[Name: {self._name}, health: {self._health}, items: {self._items}, weapon: {self._weapon}]'
+    def get_damage(self):
+        return self._damage
 
+#===================================================================================
 class Item(Entity):
     def __init__(self, name, typeOfEntity = "Entity", damage = 0, durability = 0):
-        super().__init__(name, "Item")
+        super().__init__(name)
         self._durability = durability
         self._damage = damage
         self._type = typeOfEntity
     def __repr__(self):
         return f'Item[Name: {self._name}, type: {self._type}]'
 
+#===================================================================================
 class Quest:
     def __init__(self, name):
         self._name = name
--- main.py
+++ main.py
@@ -26,6 +26,15 @@ def print_player_classes():
     print("4. Ogr")
     print("5. Knight")
 
+def print_player_classes_description():
+    print("1. Archer")
+    print(f"\tYou posses a bow that can hit from a distance. Health: {Entities.Archer.get_health(Entities.Archer(None))}. "
+          f"Damage: {Entities.Archer.get_damage(Entities.Archer(None))}")
+    print("2. Wizard")
+    print("3. Gnome")
+    print("4. Ogr")
+    print("5. Knight")
+
 def read_player_name():
     while (player_name := input("What is your name? ")) == "":
         continue
@@ -35,19 +44,30 @@ def read_player_class():
     try:
         while not (player_class := int(input("What class do you want to play? (Enter a number) "))):
             continue
-        match player_class:
-            case 1: player_class = "Archer"
-            case 2: player_class = "Wizard"
-            case 3: player_class = "Gnome"
-            case 4: player_class = "Ogr"
-            case 5: player_class = "Knight"
-            case _: raise ValueError("Wrong player_class. Usage: 1 <= player_class <= 5")
+        if not ( 1 <= player_class <= 5):
+            raise ValueError("Wrong player_class. Usage: 1 <= player_class <= 5")
         return player_class
     except ValueError:
         return read_player_class()
 
+def greeting():
+    print("Welcome to the game of D&D Light")
+    print("The concept of the game is the same as in the D&D")
+    print("The game consists of some random amount of quest that you have to complete")
+    print("Then you enter the boss stage and that's all")
+    print("Pretty simple, isn't it?")
+    print("So let's try this game out!")
+
+def separator():
+    print("====================================")
+
 def main():
     log()
+    greeting()
+    separator()
+    print("Here are the classes that you can play in this game: ")
+    print_player_classes()
+    print("Each class has its own speciality")
     player_name = read_player_name()
     print_player_classes()
     player_class = read_player_class()
